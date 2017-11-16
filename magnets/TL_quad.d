7731c0a6f788b10c02f23bcc1d930dacdfa4ae44
orblancog <orblancog@gmail.com>
2017-11-16 18:57:09 +0100

    better optics from s=55 to 90 m and FL1TE001 ok

--- magnets/TL_quad.d
+++ magnets/TL_quad.d
@@ -52,7 +52,7 @@ call, file="magnets/quadfamilies.d";
  QUATE004: quadrupole,l := ltbq/2, K1 := kte04;
  QUATE005: quadrupole,l := ltbq/2, K1 := -kte05;
  QUATE006: quadrupole,l := ltbq/2, K1 := -kte06;
- QUATE007: quadrupole,l := ltbq/2, K1 := kte07;
+ QUATE007: quadrupole,l := ltbq/2, K1 := -kte07;
  QUATE008: quadrupole,l := ltbq/2, K1 := -kte08;
  QUATE009: quadrupole,l := ltbq/2, K1 := -kte09;
 
