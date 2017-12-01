ea0272b9c4dd58c3f50e7c3d6fe0fa976e3f32f9
orblancog <orblancog@gmail.com>
2017-12-01 21:18:50 +0100

    more or less ok for the tle xy dispersion

--- magnets/TL_quad.d
+++ magnets/TL_quad.d
@@ -41,7 +41,7 @@ pertp   =  1;
 
  QUATT006: quadrupole,l := ltbq/2, K1 := ktt06;
 
- QUATT007: quadrupole,l := ltbq/2, K1 := ktt07;
+ QUATT007: quadrupole,l := ltbq/2, K1 :=  ktt07;
  QUATT008: quadrupole,l := ltbq/2, K1 := -ktt08;
  QUATT009: quadrupole,l := ltbq/2, K1 := -ktt09;
  QUATT010: quadrupole,l := ltbq/2, K1 := -ktt10;
